f69981c53e88cbb97256578c55831ba8289768f4
Nicholas Angeli <nicangeli@gmail.com>
2014-01-27 13:55:44 +0000

    Initial Commit

---
+++ Which/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Which
+//
+//  Created by Nicholas Angeli on 27/01/2014.
+//  Copyright (c) 2014 NicholasAngeli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WhichTests/WhichTests.m
@@ -0,0 +1,34 @@
+//
+//  WhichTests.m
+//  WhichTests
+//
+//  Created by Nicholas Angeli on 27/01/2014.
+//  Copyright (c) 2014 NicholasAngeli. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WhichTests : XCTestCase
+
+@end
+
+@implementation WhichTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
